1484a63b22d1a597c3d8e834417e0f66e9d81b48
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-09-09 20:18:17 -0400

    Refactor

--- sqlite/sqlite_test.go
+++ sqlite/sqlite_test.go
@@ -15,26 +15,40 @@ func TestCreate(t *testing.T) {
 	sq.OpenDB("./junk.db")
 	defer sq.Close()
 
-	sq.Exec("create table junk (a int, b text)")
-	sq.Exec("insert into junk (a,b) values (3,'test')")
+	sq.Exec(`
 
-	rows := sq.Query("select a,b from junk")
+create table IF NOT EXISTS junk (a int, 
+b datetime,
+timeEnter DATE);
+
+CREATE TRIGGER  IF NOT EXISTS insert_junk_timeEnter AFTER  INSERT ON junk
+     BEGIN
+      UPDATE junk SET timeEnter = DATETIME('NOW')  WHERE rowid = new.rowid;
+     END;
+
+`)
+	sq.Exec("insert into junk (a,b) values (3,'2013-10-07 04:23:19.120-04:00')")
+
+	rows := sq.Query("select a,b,timeEnter from junk")
 	defer rows.Close()
 	for rows.Next() {
 		var a int
 		var b string
-		err := rows.Scan(&a, &b)
+		var timeEnter string
+		err := rows.Scan(&a, &b, &timeEnter)
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		if a != 3 {
 			t.Fatalf("a value not correct")
 		}
-		if b != "test" {
-			t.Fatalf("b value not correct")
+		
+		if b != "2013-10-07T04:23:19.12-04:00" {
+			t.Fatalf("b value not correct: %v\n", b)
 		}
 
-		fmt.Println(a, b)
+		fmt.Println(a, b, timeEnter)
 	}
 	err := rows.Err()
 	if err != nil {
