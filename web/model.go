2df44e37c041d5776717037bb72b745374293f75
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-09-10 19:49:20 -0400

    Switching to gog

---
+++ web/model.go
@@ -0,0 +1,72 @@
+package web
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type product struct {
+	ID    int     `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+func (p *product) getProduct(db *sql.DB) error {
+	return db.QueryRow("SELECT name, price FROM products WHERE id=$1",
+		p.ID).Scan(&p.Name, &p.Price)
+}
+
+func (p *product) updateProduct(db *sql.DB) error {
+	_, err :=
+		db.Exec("UPDATE products SET name=$1, price=$2 WHERE id=$3",
+			p.Name, p.Price, p.ID)
+
+	return err
+}
+
+func (p *product) deleteProduct(db *sql.DB) error {
+	_, err := db.Exec("DELETE FROM products WHERE id=$1", p.ID)
+
+	return err
+}
+
+func (p *product) createProduct(db *sql.DB) error {
+
+	fmt.Printf("\n\n***************\n\n")
+	stmt := fmt.Sprintf("INSERT INTO products(name, price) VALUES ('%v', %v)", p.Name,
+		p.Price)
+
+	fmt.Printf("\n\n********%v*******\n\n", stmt)
+	_, err := db.Exec(stmt)
+
+	if err != nil {
+		fmt.Printf("...error...%v\n", err)
+		return err
+	}
+
+	return nil
+}
+
+func getProducts(db *sql.DB, start, count int) ([]product, error) {
+	rows, err := db.Query(
+		"SELECT id, name, price FROM products LIMIT $1 OFFSET $2",
+		count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	products := []product{}
+
+	for rows.Next() {
+		var p product
+		if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+
+	return products, nil
+}
